bfde272993112cc0fc192f8f21f60dc52dc9ec83
wiiiii123 <hong95402@st.vimaru.edu.vn>
2026-06-01 04:42:06 +0700

    refactor: extract clinical record panels

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,7 +6,6 @@ import { useAuditState } from "./features/audit/auditState.js";
 import { AuthenticatedLayout } from "./components/AppShell.js";
 import { buildClinicalDocumentHandlers } from "./features/clinical-documents/clinicalDocumentHandlers.js";
 import { buildClinicalDocumentPanelRenderers } from "./features/clinical-documents/clinicalDocumentPanelRenderers.js";
-import { buildClinicalRecordPanelRenderers } from "./features/clinical-records/clinicalRecordPanelRenderers.js";
 import { useClinicalRecordState } from "./features/clinical-records/clinicalRecordState.js";
 import { buildCarePlanHandlers } from "./features/clinical-records/carePlanHandlers.js";
 import { buildMedicationHandlers } from "./features/clinical-records/medicationHandlers.js";
@@ -41,6 +40,7 @@ import {
 import { useAppLifecycleEffects } from "./pages/appLifecycleEffects.js";
 import { useAppShellState } from "./pages/appShellState.js";
 import { buildAppRoutePanels } from "./pages/appRoutePanels.js";
+import { buildClinicalRecordPanels } from "./pages/clinicalRecordPanelContext.js";
 
 import {
   defaultTransferContext,
@@ -490,22 +490,8 @@ export function App() {
     onReloadGlobalAuditEvents: loadGlobalAuditEvents,
     onVerifyAuditIntegrity: verifyAuditIntegrity
   });
-  const clinicalRecordPanels = buildClinicalRecordPanelRenderers({
-    collections: patientWorkspaceCollections,
-    forms: {
-      allergyIntolerance: clinicalRecordState.allergyIntoleranceForm,
-      condition: clinicalRecordState.conditionForm,
-      diagnosticReport: clinicalRecordState.diagnosticReportForm,
-      encounter: clinicalRecordState.encounterForm,
-      imagingStudy: clinicalRecordState.imagingStudyForm,
-      medicationAdministration:
-        clinicalRecordState.medicationAdministrationForm,
-      medicationDispense: clinicalRecordState.medicationDispenseForm,
-      medicationRequest: clinicalRecordState.medicationRequestForm,
-      observation: clinicalRecordState.observationForm,
-      procedure: clinicalRecordState.procedureForm,
-      serviceRequest: clinicalRecordState.serviceRequestForm
-    },
+  const clinicalRecordPanels = buildClinicalRecordPanels({
+    clinicalRecordState,
     handlers: {
       onCreateAllergyIntolerance: handleCreateAllergyIntolerance,
       onCreateCondition: handleCreateCondition,
@@ -551,67 +537,9 @@ export function App() {
       onSelectServiceRequest: clinicalRecordState.setSelectedServiceRequestId,
       onSelectWorkflowTask: clinicalRecordState.setSelectedWorkflowTaskId
     },
-    isFinishingEncounter: clinicalRecordState.isFinishingEncounter,
     isWriteDisabled: selectedPatientWriteDisabled,
-    loading: {
-      allergyIntolerances: clinicalRecordState.isLoadingAllergyIntolerances,
-      conditions: clinicalRecordState.isLoadingConditions,
-      diagnosticReports: clinicalRecordState.isLoadingDiagnosticReports,
-      encounters: clinicalRecordState.isLoadingEncounters,
-      imagingStudies: clinicalRecordState.isLoadingImagingStudies,
-      medicationAdministrations:
-        clinicalRecordState.isLoadingMedicationAdministrations,
-      medicationDispenses: clinicalRecordState.isLoadingMedicationDispenses,
-      medicationRequests: clinicalRecordState.isLoadingMedicationRequests,
-      observations: clinicalRecordState.isLoadingObservations,
-      procedures: clinicalRecordState.isLoadingProcedures,
-      serviceRequests: clinicalRecordState.isLoadingServiceRequests,
-      workflowTasks: clinicalRecordState.isLoadingWorkflowTasks
-    },
-    selectedIds: {
-      allergyIntolerance: clinicalRecordState.selectedAllergyIntoleranceId,
-      condition: clinicalRecordState.selectedConditionId,
-      diagnosticReport: clinicalRecordState.selectedDiagnosticReportId,
-      encounter: clinicalRecordState.selectedEncounterId,
-      imagingStudy: clinicalRecordState.selectedImagingStudyId,
-      medicationAdministration:
-        clinicalRecordState.selectedMedicationAdministrationId,
-      medicationDispense: clinicalRecordState.selectedMedicationDispenseId,
-      medicationRequest: clinicalRecordState.selectedMedicationRequestId,
-      observation: clinicalRecordState.selectedObservationId,
-      procedure: clinicalRecordState.selectedProcedureId,
-      serviceRequest: clinicalRecordState.selectedServiceRequestId,
-      workflowTask: clinicalRecordState.selectedWorkflowTaskId
-    },
-    selections: {
-      selectedAllergyIntolerance: workspaceSelection.selectedAllergyIntolerance,
-      selectedCondition: workspaceSelection.selectedCondition,
-      selectedDiagnosticReport: workspaceSelection.selectedDiagnosticReport,
-      selectedEncounter: workspaceSelection.selectedEncounter,
-      selectedEncounterCounts: workspaceSelection.selectedEncounterCounts,
-      selectedImagingStudy: workspaceSelection.selectedImagingStudy,
-      selectedMedicationAdministration: workspaceSelection.selectedMedicationAdministration,
-      selectedMedicationDispense: workspaceSelection.selectedMedicationDispense,
-      selectedMedicationRequest: workspaceSelection.selectedMedicationRequest,
-      selectedObservation: workspaceSelection.selectedObservation,
-      selectedProcedure: workspaceSelection.selectedProcedure,
-      selectedServiceRequest: workspaceSelection.selectedServiceRequest,
-      selectedWorkflowTask: workspaceSelection.selectedWorkflowTask
-    },
-    submitting: {
-      allergyIntolerance: clinicalRecordState.isSubmittingAllergyIntolerance,
-      condition: clinicalRecordState.isSubmittingCondition,
-      diagnosticReport: clinicalRecordState.isSubmittingDiagnosticReport,
-      encounter: clinicalRecordState.isSubmittingEncounter,
-      imagingStudy: clinicalRecordState.isSubmittingImagingStudy,
-      medicationAdministration:
-        clinicalRecordState.isSubmittingMedicationAdministration,
-      medicationDispense: clinicalRecordState.isSubmittingMedicationDispense,
-      medicationRequest: clinicalRecordState.isSubmittingMedicationRequest,
-      observation: clinicalRecordState.isSubmittingObservation,
-      procedure: clinicalRecordState.isSubmittingProcedure,
-      serviceRequest: clinicalRecordState.isSubmittingServiceRequest
-    }
+    patientWorkspaceCollections,
+    workspaceSelection
   });
   const clinicalDocumentPanels = buildClinicalDocumentPanelRenderers({
     clinicalDocuments: clinicalRecordState.clinicalDocuments,
